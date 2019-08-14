399de03a4fdba44396cdc44436dc7c180ad6d088
long charmroeun <longcharmroeun@gmail.com>
2019-08-14 09:15:41 +0700

    Test Debuger PHP

--- JavaScrip Lesson/Caculator/JavaScript1.js
+++ JavaScrip Lesson/Caculator/JavaScript1.js
@@ -3,7 +3,7 @@
         document.getElementById("sc").value ="";
     }
     else if (text.innerText == "=") {
-        document.getElementById("sc").value += "=" + eval(document.getElementById("sc").value);
+        document.getElementById("sc").value = eval(document.getElementById("sc").value);
     }
     else {
         document.getElementById("sc").value += text.innerText;asd
