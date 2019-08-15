9f0c86f8bd8d64c26d3ff0ca5d7b7de945f3fae3
long charmroeun <longcharmroeun@gmail.com>
2019-08-15 13:07:44 +0700

    Design dire

---
+++ HomeWork/JavaScrip/HomeWork No.1/JavaScript1.js
@@ -0,0 +1,27 @@
+﻿function No1() {
+    var name = prompt("What's is your name:");
+    if (name != null) {
+        alert("Your Name Is :" + name);
+    }
+}
+
+function No2() {
+    var name = prompt("What's is your year of birth:",new Date().getFullYear());
+    if (name != null) {
+        alert("Your age is :" + (new Date().getFullYear() - Number(name)));
+    }
+}
+
+function No3() {
+    var name = prompt("length of the square:");
+    if (name != null) {
+        alert("Perimeter:" + (Number(name) * 4));
+    }
+}
+
+function No4() {
+    var name = prompt("circle radius:");
+    if (name != null) {
+        alert("Area of circle:" + Math.pow((Number(name)), 2) * 3.14);
+    }
+}
