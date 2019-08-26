23d3fd0441d3911efcd4371e1ffc4c4558438fd1
long charmroeun <longcharmroeun@gmail.com>
2019-08-26 09:27:21 +0700

    Add table.js

---
+++ JavaScrip Lesson/Table/Table.js
@@ -0,0 +1,31 @@
+﻿Header = new Array();
+function Load() {
+    Header.push({ Key: "ID", ClassName: "Row Head QTY Body" });
+}
+
+function onload() {
+    document.getElementById("header").appendChild(Colunm());
+}
+
+function Colunm() {
+    var div = document.createElement("div");
+    div.className = "Caculator";
+    for (var i = 0; i < 20; i++) {
+        div.appendChild(TableHeader("Row"));
+    }
+    return div;
+}
+
+function TableHeader(ClassName) {
+    Load();
+    var div = document.createElement("div");
+    div.className = ClassName;
+    for (var i = 0; i < 6; i++) {
+        var label = document.createElement("input");
+        label.className = Header[0].ClassName;
+
+        div.appendChild(label);
+    }
+
+    return div
+}
